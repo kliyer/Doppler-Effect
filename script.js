d36c8ac3297fe89f427917ba9393eec6344592ca
Kartik Iyer <iyerkartik04@gmail.com>
2016-04-10 13:58:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ function drawReferenceImage(imageObj1, number) {
 		
 	context.drawImage(imageObj, 0, 0);
 	var imageData= context.getImageData(0, 0, imageWidth, imageHeight);
-    	var tempdata = imageData.data; 
+    	var tempdata = imageData.data;
+    	//copying image data in an array
 	for (var i = 0, n = tempdata.length; i < n; i++) {
-			data[i]= tempdata [i];
+		data[i]= tempdata [i];
 	}
 }
 function modifyImage(imageObj1, number) {
@@ -24,7 +25,7 @@ function modifyImage(imageObj1, number) {
 	factor = Math.log10 ( Math.abs (number));
 	var imageData1 = context.getImageData(imageX, imageY, imageWidth, imageHeight);
 
-        // iterate over all pixels
+        // iterate over all pixels and modify each pixel
 	for(var i = 0, n = data.length; i < n; i += 4) {
 	        var red = data[i];
 	        var green = data[i + 1];
@@ -41,9 +42,9 @@ function modifyImage(imageObj1, number) {
 	        	imageData1.data[i + 1] = green - (number * factor);
 	        	imageData1.data[i + 2] = blue - (number * factor);
 	        	imageData1.data[i + 3] = alpha;
-			} 
+		} 
 		else {
-				imageData1.data[i] = red;
+			imageData1.data[i] = red;
 	          	imageData1.data[i + 1] = green;
 	          	imageData1.data[i + 2] = blue;
 	          	imageData1.data[i + 3] = alpha;
@@ -57,7 +58,6 @@ drawReferenceImage(this, 0)};
 var diff = 0;
 $('#slider').val(0);
 $('#ip').val(0);
-imageObj.crossOrigin = 'anonymous';
 imageObj.src = 'star-small.png';
 
 $( document ).ready(function() {
@@ -67,24 +67,20 @@ $( document ).ready(function() {
 	    	modifyImage(this, $('#slider').val());
 	    };
 		$('#ip').val($('#slider').val());
-		imageObj.crossOrigin = 'anonymous';
 		imageObj.src = 'star-small.png';
 	});
 
 	$('#ip').change(function() {
-		if($('#ip').val() < -100 || $('#ip').val() > 100){
+		if($('#ip').val() < -100 || $('#ip').val() > 100) {
 			alert('Value should be within range -100 to 100');
-			$('#ip').val();
 		} 
-		else{
-		    	var imageObj = new Image();
-		    	imageObj.onload = function() {
-		   			modifyImage(this, $('#ip').val());
-		   		};
+		else {
+			var imageObj = new Image();
+			imageObj.onload = function() {
+		   		modifyImage(this, $('#ip').val());
+		   	};
 			$('#slider').val($('#ip').val());
-			imageObj.crossOrigin = 'anonymous';
 			imageObj.src = 'star-small.png';
 		}
 	});
 });
-
